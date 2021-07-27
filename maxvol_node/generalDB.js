a79eaefdf22ec870f378fac30af703d17a49d430
Lozan <lozan2312@gmail.com>
2021-07-27 12:29:28 +0300

    'update'

--- maxvol_node/generalDB.js
+++ maxvol_node/generalDB.js
@@ -3,11 +3,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/VolData";
 
-const Binance = require('node-binance-api');
-const binance = new Binance().options({
-  APIKEY: 'ZmqVNqnYg58E9O2S6IKKVOVkVbsI9UBoKE5XNyaa8J0biSQAEVnpI6YA7sVnayht',
-  APISECRET: 'FRQlQvuZoGofkPXmcv795mEwDvYkys4X8zZa5IITJnAkJ587HEZTzHLazBfBsT2z'
-});
+
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
--- maxvol_node/index.js
+++ maxvol_node/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
 const port = 8000;
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: 'ZmqVNqnYg58E9O2S6IKKVOVkVbsI9UBoKE5XNyaa8J0biSQAEVnpI6YA7sVnayht',
+  APISECRET: 'FRQlQvuZoGofkPXmcv795mEwDvYkys4X8zZa5IITJnAkJ587HEZTzHLazBfBsT2z'
+});
 
 
 app.listen(port, () => {
