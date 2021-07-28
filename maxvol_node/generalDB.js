4bbdb1e118d1ccaaff7d2427825681e4b84c1fcc
Lozan <lozan2312@gmail.com>
2021-07-28 16:09:41 +0300

    'update28/07'

--- maxvol_node/generalDB.js
+++ maxvol_node/generalDB.js
@@ -4,9 +4,14 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/VolData";
 
 
-
-MongoClient.connect(url, function(err, db) {
+module.exports.MongFunc = async(symbols)=>{
+  MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var dbo = db.db("VolData");
+  // dbo.collection("CoinPair").insertMany(symbols, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("Number of documents inserted: " + res.insertedCount);
+  //   db.close();
+  // });
 });
+}
--- maxvol_node/index.js
+++ maxvol_node/index.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express();
 const port = 8000;
 const Binance = require('node-binance-api');
+const mongodb = require('./generalDB.js');
+
 const binance = new Binance().options({
   APIKEY: 'ZmqVNqnYg58E9O2S6IKKVOVkVbsI9UBoKE5XNyaa8J0biSQAEVnpI6YA7sVnayht',
   APISECRET: 'FRQlQvuZoGofkPXmcv795mEwDvYkys4X8zZa5IITJnAkJ587HEZTzHLazBfBsT2z'
 });
+
+async function CoinFunc(){
+  await binance.bookTickers((error, ticker) => {
+  var CoinPair=[]
+  let coins=ticker
+  for (let i = 0; i < coins.length; i++) {
+     CoinPair.push(coins[i]["symbol"]);
+  }
+  let Symbol=JSON.parse('[{"symbol":'+JSON.stringify(CoinPair)+'}]')
+  mongodb.MongFunc(Symbol)
+});
+}
 
 
 app.listen(port, () => {
